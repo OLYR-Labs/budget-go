01091c7f73eb8f69c65ef25c55358112fdaaca1d
banukadilshan-dissanayaka <banukadilshan078@gmail.com>
2026-08-26 02:19:46 +0530

    feat: mount push notification setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { PushSetup } from "@/components/notifications/push-setup";
 
 export const metadata: Metadata = {
   title: {
@@ -33,6 +34,7 @@ export default function RootLayout({
           disableTransitionOnChange={false}
         >
           {children}
+          <PushSetup />
         </ThemeProvider>
       </body>
     </html>
